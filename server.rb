1ee654fe46d6d781cb2297ce84bf3219721ac45b
Roope Merikukka <roope.merikukka@bond.fi>
2015-01-10 13:33:41 +0200

    change dir in the same command with git action

--- server.rb
+++ server.rb
@@ -13,12 +13,10 @@ $clone = "git@github.com:roopemerikukka/aatu-web.git"
 
 get '/aatu-web' do
   if File.exists?("#{$repo_path}/#{$repo_name}")
-    system("cd #{$repo_path}")
-    system("git pull")
+    system("cd #{$repo_path} && git pull")
     puts "pulled"
   else
-    system("cd #{$repo_path}")
-    system("git clone #{$clone}")
+    system("cd #{$repo_path} && git clone #{$clone}")
     puts "cloned"
   end
 end
